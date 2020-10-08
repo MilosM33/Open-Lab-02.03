4c4b139299cc8c5c967c6bdafc3a2fa03b7a3fc6
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 13:28:44 +0200

    Init

--- Open-Lab-02.03/Calculator.cs
+++ Open-Lab-02.03/Calculator.cs
@@ -4,9 +4,7 @@ namespace Open_Lab_02._03
 {
     public class Calculator
     {
-        public int Remainder(int dividend, int divisor)
-        {
-            throw new NotImplementedException();
-        }
+        public int Remainder(int dividend, int divisor) => dividend % divisor;
+        
     }
 }
